b65c436cf1cc591c0b794e161cb956b53d046a62
Nikita Pinegin <nobodyni@icloud.com>
2024-09-08 02:39:00 +0300

    Добавил файл about.html, маршруты и 2 функции
    about.html и index.html наследуются от базового шаблона base.html и
    выводят текст: Главная страница и О сайте
    
    в файле views.py добавил 2 функции представления
    
    в файле urls.py добавил 2 маршрута для функций

--- developer/views.py
+++ developer/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    pass
+    data = {
+        'title': 'Главная страница',
+    }
+    return render(request, 'developer/index.html', context=data)
+
+
+def about(request):
+    data = {
+        'title': 'О сайте',
+    }
+    return render(request, 'developer/about.html', context=data)
