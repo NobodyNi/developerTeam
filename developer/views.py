d6d615892b6b5aa231e2036073341bac02cfa84d
Nikita Pinegin <nobodyni@icloud.com>
2024-09-08 02:57:10 +0300

    Добавил навигацию по страницам

--- developer/views.py
+++ developer/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+menu = [{'title': 'Главная страница', 'url_name': 'home'},
+        {'title': 'О сайте', 'url_name': 'about'}]
+
 
 def index(request):
     data = {
         'title': 'Главная страница',
+        'menu': menu,
     }
     return render(request, 'developer/index.html', context=data)
 
@@ -11,5 +15,6 @@ def index(request):
 def about(request):
     data = {
         'title': 'О сайте',
+        'menu': menu,
     }
     return render(request, 'developer/about.html', context=data)
