0711b65c919046938c2088a1b6941023c15e406c
Nikita Pinegin <nobodyni@icloud.com>
2024-09-10 01:18:26 +0300

    Добавил модель developer
    Отображает название урока и его содержимое

--- developer/models.py
+++ developer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Developer(models.Model):
+    lesson = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=125, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.lesson
+
+
+
+
