992c102dd8cd2015a43d14ab86af98c13797415a
Nikita Pinegin <nobodyni@icloud.com>
2024-09-16 01:43:35 +0300

    Добавил таблицу SubDeveloper в админ-панель

--- developer/admin.py
+++ developer/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Developer
+from .models import Developer, SubDeveloper
 
 
 @admin.register(Developer)
 class DeveloperAdmin(admin.ModelAdmin):
     list_display = ['lesson', 'content', 'time_create', 'is_published', 'slug']
-    list_display_links = ['lesson']
-    list_editable = ['content']
+    list_editable = ['content', 'is_published', 'slug']
+    search_fields = ['lesson']
+
+
+@admin.register(SubDeveloper)
+class SubDeveloperAdmin(admin.ModelAdmin):
+    list_display = ['lesson', 'content', 'slug']
+    list_editable = ['content', 'slug']
+    search_fields = ['lesson']
 
 
 
--- developer/models.py
+++ developer/models.py
@@ -3,12 +3,12 @@ from django.urls import reverse
 
 
 class Developer(models.Model):
-    lesson = models.CharField(max_length=100)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    slug = models.SlugField(max_length=125, unique=True, db_index=True)
+    lesson = models.CharField(max_length=100, verbose_name='Тема')
+    content = models.TextField(blank=True, verbose_name='Информация')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    is_published = models.BooleanField(default=True, verbose_name='Статус')
+    slug = models.SlugField(max_length=125, unique=True, db_index=True, verbose_name='Слаг')
 
     direct = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
     sub_lesson = models.ManyToManyField('SubDeveloper', blank=True, related_name='sub')
@@ -21,18 +21,24 @@ class Developer(models.Model):
 
     class Meta:
         ordering = ['time_create']
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
 
 
 class SubDeveloper(models.Model):
-    lesson = models.CharField(max_length=100)
-    content = models.TextField(blank=True)
-    slug = models.SlugField(max_length=125, db_index=True, unique=True)
+    lesson = models.CharField(max_length=100, verbose_name='Подтема')
+    content = models.TextField(blank=True, verbose_name='Информация')
+    slug = models.SlugField(max_length=125, db_index=True, unique=True, verbose_name='Слаг')
 
     def __str__(self):
         return self.lesson
 
     def get_absolute_url(self):
         return reverse('sub_lesson', kwargs={'sub_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Подтема'
+        verbose_name_plural = 'Подтемы'
 
 
 class Category(models.Model):
