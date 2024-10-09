88ac52e465bb5ebf82c9bb0699c186049cd845ed
Nikita Pinegin <nobodyni@icloud.com>
2024-10-09 14:43:59 +0300

    .

--- developerTeam/settings.py
+++ developerTeam/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-wp53cs5a30=)+=xyu58-%liuzl+gg1ywn*fc-$79v)^$dzumol'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
