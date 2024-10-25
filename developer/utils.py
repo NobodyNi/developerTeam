2858f8e1c8998b525857e499acd30bf1066c4bca
Nikita Pinegin <nobodyni@icloud.com>
2024-10-26 01:55:59 +0300

    Добавил новый миксин, убрал дублирование кода

--- developer/utils.py
+++ developer/utils.py
@@ -1,5 +1,15 @@
+from django.shortcuts import get_object_or_404
+
+from developer.models import Developer, CategoryOOP, CategoryAsync, CategoryAlgoritm
+
+
 class PostMixin:
     lesson_attr = None
+
+    def get_queryset(self):
+        slug_field = self.slug_field
+        slug = self.kwargs[slug_field]
+        return [get_object_or_404(self.model, slug=slug)]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -9,3 +19,19 @@ class PostMixin:
         if self.lesson_attr:
             context['lesson'] = getattr(post, self.lesson_attr).all().order_by('pk')
         return context
+
+
+class SlugMappingMixin:
+    slug_to_model = {
+        'python': Developer,
+        'python-oop': CategoryOOP,
+        'python-async': CategoryAsync,
+        'python-algoritm': CategoryAlgoritm
+    }
+
+    slug_to_template = {
+        'python': 'developer/category.html',
+        'python-oop': 'developer/category_oop.html',
+        'python-async': 'developer/async_py.html',
+        'python-algoritm': 'developer/algoritm_py.html'
+    }
--- developer/views.py
+++ developer/views.py
@@ -7,7 +7,7 @@ from .services import random_lesson_slug
 
 from developer.models import Category, Developer, CategoryOOP, SubDeveloper, SubCategoryOOP, SubCategoryAlgoritm, \
     CategoryAlgoritm, CategoryAsync, SubCategoryAsync
-from .utils import PostMixin
+from .utils import PostMixin, SlugMappingMixin
 
 
 class IndexView(TemplateView):
@@ -19,24 +19,15 @@ class IndexView(TemplateView):
         return context
 
 
-class DirectionView(View):
+class DirectionView(SlugMappingMixin, View):
     def get(self, request, dir_slug):
         category = get_object_or_404(Category, slug=dir_slug)
-
-        if dir_slug == 'python':
-            posts = Developer.objects.filter(is_published=1).order_by('time_create')
-            template_name = 'developer/category.html'
-        elif dir_slug == 'python-oop':
-            posts = CategoryOOP.objects.filter(is_published=1).order_by('time_create')
-            template_name = 'developer/category_oop.html'
-        elif dir_slug == 'python-async':
-            posts = CategoryAsync.objects.filter(is_published=1).order_by('time_create')
-            template_name = 'developer/async_py.html'
-        elif dir_slug == 'python-algoritm':
-            posts = CategoryAlgoritm.objects.filter(is_published=1).order_by('time_create')
-            template_name = 'developer/algoritm_py.html'
-        else:
+        model = self.slug_to_model.get(dir_slug)
+        if model is None:
             return HttpResponse('Страница не найдена')
+
+        posts = model.objects.filter(is_published=1).order_by('time_create')
+        template_name = self.slug_to_template[dir_slug]
 
         context = {
             'title': category.direction,
@@ -50,73 +41,49 @@ class PostView(PostMixin, ListView):
     model = Developer
     template_name = 'developer/post.html'
     lesson_attr = 'sub_lesson'
-
-    def get_queryset(self):
-        post_slug = self.kwargs['post_slug']
-        return [get_object_or_404(Developer, slug=post_slug)]
+    slug_field = 'post_slug'
 
 
 class SubPostView(PostMixin, ListView):
     model = SubDeveloper
     template_name = 'developer/post.html'
-
-    def get_queryset(self):
-        sub_slug = self.kwargs['sub_slug']
-        return [get_object_or_404(SubDeveloper, slug=sub_slug)]
+    slug_field = 'sub_slug'
 
 
 class PostViewOOP(PostMixin, ListView):
     model = CategoryOOP
     template_name = 'developer/post_oop.html'
     lesson_attr = 'sub_lesson_oop'
-
-    def get_queryset(self):
-        cat_oop_slug = self.kwargs['cat_oop_slug']
-        return [get_object_or_404(CategoryOOP, slug=cat_oop_slug)]
+    slug_field = 'cat_oop_slug'
 
 
 class SubPostViewOOP(PostMixin, ListView):
     model = SubCategoryOOP
     template_name = 'developer/post_oop.html'
-
-    def get_queryset(self):
-        sub_slug = self.kwargs['sub_slug']
-        return [get_object_or_404(SubCategoryOOP, slug=sub_slug)]
+    slug_field = 'sub_slug'
 
 
 class PostViewAlgoritm(PostMixin, ListView):
     model = CategoryAlgoritm
     template_name = 'developer/post_algoritm.html'
     lesson_attr = 'sub_lesson_algoritm'
-
-    def get_queryset(self):
-        algoritm_slug = self.kwargs['algoritm_slug']
-        return [get_object_or_404(CategoryAlgoritm, slug=algoritm_slug)]
+    slug_field = 'algoritm_slug'
 
 
 class SubPostViewAlgoritm(PostMixin, ListView):
     model = SubCategoryAlgoritm
     template_name = 'developer/post_algoritm.html'
-
-    def get_queryset(self):
-        sub_algoritm = self.kwargs['sub_algoritm']
-        return [get_object_or_404(SubCategoryAlgoritm, slug=sub_algoritm)]
+    slug_field = 'sub_algoritm'
 
 
 class PostViewAsync(PostMixin, ListView):
     model = CategoryAsync
     template_name = 'developer/post_async.html'
     lesson_attr = 'sub_lesson_async'
-
-    def get_queryset(self):
-        async_slug = self.kwargs['async_slug']
-        return [get_object_or_404(CategoryAsync, slug=async_slug)]
+    slug_field = 'async_slug'
 
 
 class SubPostViewAsync(PostMixin, ListView):
     model = SubCategoryAsync
     template_name = 'developer/post_async.html'
-
-    def get_queryset(self):
-        sub_async = self.kwargs['sub_async']
-        return [get_object_or_404(SubCategoryAsync, slug=sub_async)]
+    slug_field = 'sub_async'
