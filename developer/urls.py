8afd8e5d2615ada92d63e91e3ec911eeb1ec4908
Nikita Pinegin <nobodyni@icloud.com>
2024-10-25 20:05:39 +0300

    Заменил функции на классы

--- developer/urls.py
+++ developer/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from . import views
+from .views import IndexView, DirectionView, PostView, SubPostView, PostViewOOP, SubPostViewOOP, PostViewAlgoritm, \
+    SubPostViewAlgoritm, PostViewAsync, SubPostViewAsync
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    # path('about/', views.about, name='about'),
-    path('direction/<slug:dir_slug>/', views.show_direction, name='direction'),
-    path('posts/<slug:post_slug>/', views.show_post, name='posting'),
-    path('posts/sub/<slug:sub_slug>/', views.show_sub_lesson, name='sub_lesson'),
-    path('posts/oop/<slug:cat_oop_slug>/', views.show_post_oop, name='posting_oop'),
-    path('posts/oop/sub/<slug:sub_slug>/', views.show_sub_lesson_oop, name='sub_lesson_oop'),
-    path('posts/algoritm/<slug:algoritm_slug>/', views.show_post_algoritm, name='posting_algoritm'),
-    path('posts/algoritm/sub/<slug:sub_algoritm>/', views.show_sub_lesson_algoritm, name='posting_sub_algoritm'),
-    path('posts/async/<slug:async_slug>/', views.show_post_async, name='posting_async'),
-    path('posts/async/sub/<slug:sub_async>/', views.show_sub_lesson_async, name='posting_sub_async'),
+    path('', IndexView.as_view(), name='home'),
+    path('direction/<slug:dir_slug>/', DirectionView.as_view(), name='direction'),
+    path('posts/<slug:post_slug>/', PostView.as_view(), name='posting'),
+    path('posts/sub/<slug:sub_slug>/', SubPostView.as_view(), name='sub_lesson'),
+    path('posts/oop/<slug:cat_oop_slug>/', PostViewOOP.as_view(), name='posting_oop'),
+    path('posts/oop/sub/<slug:sub_slug>/', SubPostViewOOP.as_view(), name='sub_lesson_oop'),
+    path('posts/algoritm/<slug:algoritm_slug>/', PostViewAlgoritm.as_view(), name='posting_algoritm'),
+    path('posts/algoritm/sub/<slug:sub_algoritm>/', SubPostViewAlgoritm.as_view(), name='posting_sub_algoritm'),
+    path('posts/async/<slug:async_slug>/', PostViewAsync.as_view(), name='posting_async'),
+    path('posts/async/sub/<slug:sub_async>/', SubPostViewAsync.as_view(), name='posting_sub_async'),
 ]
--- developer/views.py
+++ developer/views.py
@@ -1,139 +1,176 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.views.generic import TemplateView, ListView
+
 from .services import random_lesson_slug
 
 from developer.models import Category, Developer, CategoryOOP, SubDeveloper, SubCategoryOOP, SubCategoryAlgoritm, \
     CategoryAlgoritm, CategoryAsync, SubCategoryAsync
 
 
-# menu = [{'title': 'Главная страница', 'url_name': 'home'},
-#         {'title': 'О сайте', 'url_name': 'about'}]
-
-
-def index(request):
-    data = {
+class IndexView(TemplateView):
+    template_name = 'developer/index.html'
+    extra_context = {
         'random_theme': random_lesson_slug(),
     }
-    return render(request, 'developer/index.html', context=data)
 
 
-# def about(request):
-#     data = {
-#         'title': 'О сайте',
-#         'menu': menu,
-#     }
-#     return render(request, 'developer/about.html', context=data)
+class DirectionView(View):
+    def get(self, request, dir_slug):
+        category = get_object_or_404(Category, slug=dir_slug)
+
+        if dir_slug == 'python':
+            posts = Developer.objects.filter(is_published=1).order_by('time_create')
+            template_name = 'developer/category.html'
+        elif dir_slug == 'python-oop':
+            posts = CategoryOOP.objects.filter(is_published=1).order_by('time_create')
+            template_name = 'developer/category_oop.html'
+        elif dir_slug == 'python-async':
+            posts = CategoryAsync.objects.filter(is_published=1).order_by('time_create')
+            template_name = 'developer/async_py.html'
+        elif dir_slug == 'python-algoritm':
+            posts = CategoryAlgoritm.objects.filter(is_published=1).order_by('time_create')
+            template_name = 'developer/algoritm_py.html'
+        else:
+            return HttpResponse('Страница не найдена')
+
+        context = {
+            'title': category.direction,
+            'posts': posts
+        }
+
+        return render(request, template_name, context=context)
 
 
-def show_direction(request, dir_slug):
-    category = get_object_or_404(Category, slug=dir_slug)
-    data_1 = {
-        'title': category.direction,
-        'posts': Developer.objects.filter(is_published=1).order_by('time_create'),
-    }
+class PostView(ListView):
+    model = Developer
+    template_name = 'developer/post.html'
 
-    data_2 = {
-        'title': category.direction,
-        'posts': CategoryOOP.objects.filter(is_published=1).order_by('time_create'),
-    }
+    def get_queryset(self):
+        post_slug = self.kwargs['post_slug']
+        return [get_object_or_404(Developer, slug=post_slug)]
 
-    data_3 = {
-        'title': category.direction,
-        'posts': CategoryAlgoritm.objects.filter(is_published=1).order_by('time_create'),
-    }
-
-    data_4 = {
-        'title': category.direction,
-        'posts': CategoryAsync.objects.filter(is_published=1).order_by('time_create'),
-    }
-
-    if dir_slug == 'python':
-        return render(request, 'developer/category.html', context=data_1)
-    elif dir_slug == 'python-oop':
-        return render(request, 'developer/category_oop.html', context=data_2)
-    elif dir_slug == 'python-async':
-        return render(request, 'developer/async_py.html', context=data_4)
-    elif dir_slug == 'python-algoritm':
-        return render(request, 'developer/algoritm_py.html', context=data_3)
-    else:
-        return HttpResponse('Страница не найдена')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object_list[0]
+        context['title'] = post.lesson
+        context['lesson'] = post.sub_lesson.all().order_by('pk')
+        context['posts'] = post
+        return context
 
 
-def show_post(request, post_slug):
-    posts = get_object_or_404(Developer, slug=post_slug)
-    lesson = posts.sub_lesson.all().order_by('pk')
-    data = {
-        'title': posts.lesson,
-        'posts': posts,
-        'lesson': lesson,
-    }
-    return render(request, 'developer/post.html', context=data)
+class SubPostView(ListView):
+    model = SubDeveloper
+    template_name = 'developer/post.html'
+
+    def get_queryset(self):
+        sub_slug = self.kwargs['sub_slug']
+        return [get_object_or_404(SubDeveloper, slug=sub_slug)]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object_list[0]
+        context['title'] = post.lesson
+        context['posts'] = post
+        return context
 
 
-def show_post_oop(request, cat_oop_slug):
-    posts = get_object_or_404(CategoryOOP, slug=cat_oop_slug)
-    lesson = posts.sub_lesson_oop.all().order_by('pk')
-    data = {
-        'title': posts.lesson,
-        'posts': posts,
-        'lesson': lesson,
-    }
-    return render(request, 'developer/post_oop.html', context=data)
+class PostViewOOP(ListView):
+    model = CategoryOOP
+    template_name = 'developer/post_oop.html'
+
+    def get_queryset(self):
+        cat_oop_slug = self.kwargs['cat_oop_slug']
+        return [get_object_or_404(CategoryOOP, slug=cat_oop_slug)]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object_list[0]
+        context['title'] = post.lesson
+        context['lesson'] = post.sub_lesson_oop.all().order_by('pk')
+        context['posts'] = post
+        return context
 
 
-def show_sub_lesson(request, sub_slug):
-    lesson = get_object_or_404(SubDeveloper, slug=sub_slug)
-    data = {
-        'title': lesson.lesson,
-        'posts': lesson,
-    }
-    return render(request, 'developer/post.html', context=data)
+class SubPostViewOOP(ListView):
+    model = SubCategoryOOP
+    template_name = 'developer/post_oop.html'
+
+    def get_queryset(self):
+        sub_slug = self.kwargs['sub_slug']
+        return [get_object_or_404(SubCategoryOOP, slug=sub_slug)]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object_list[0]
+        context['title'] = post.lesson
+        context['posts'] = post
+        return context
 
 
-def show_sub_lesson_oop(request, sub_slug):
-    lesson = get_object_or_404(SubCategoryOOP, slug=sub_slug)
-    data = {
-        'title': lesson.lesson,
-        'posts': lesson,
-    }
-    return render(request, 'developer/post_oop.html', context=data)
+class PostViewAlgoritm(ListView):
+    model = CategoryAlgoritm
+    template_name = 'developer/post_algoritm.html'
+
+    def get_queryset(self):
+        algoritm_slug = self.kwargs['algoritm_slug']
+        return [get_object_or_404(CategoryAlgoritm, slug=algoritm_slug)]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object_list[0]
+        context['title'] = post.lesson
+        context['lesson'] = post.sub_lesson_algoritm.all().order_by('pk')
+        context['posts'] = post
+        return context
 
 
-def show_post_algoritm(request, algoritm_slug):
-    posts = get_object_or_404(CategoryAlgoritm, slug=algoritm_slug)
-    lesson = posts.sub_lesson_algoritm.all().order_by('pk')
-    data = {
-        'title': posts.lesson,
-        'posts': posts,
-        'lesson': lesson,
-    }
-    return render(request, 'developer/post_algoritm.html', context=data)
+class SubPostViewAlgoritm(ListView):
+    model = SubCategoryAlgoritm
+    template_name = 'developer/post_algoritm.html'
+
+    def get_queryset(self):
+        sub_algoritm = self.kwargs['sub_algoritm']
+        return [get_object_or_404(SubCategoryAlgoritm, slug=sub_algoritm)]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object_list[0]
+        context['title'] = post.lesson
+        context['posts'] = post
+        return context
 
 
-def show_sub_lesson_algoritm(request, sub_algoritm):
-    lesson = get_object_or_404(SubCategoryAlgoritm, slug=sub_algoritm)
-    data = {
-        'title': lesson.lesson,
-        'posts': lesson,
-    }
-    return render(request, 'developer/post_algoritm.html', context=data)
+class PostViewAsync(ListView):
+    model = CategoryAsync
+    template_name = 'developer/post_async.html'
+
+    def get_queryset(self):
+        async_slug = self.kwargs['async_slug']
+        return [get_object_or_404(CategoryAsync, slug=async_slug)]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object_list[0]
+        context['title'] = post.lesson
+        context['lesson'] = post.sub_lesson_async.all().order_by('pk')
+        context['posts'] = post
+        return context
 
 
-def show_post_async(request, async_slug):
-    posts = get_object_or_404(CategoryAsync, slug=async_slug)
-    lesson = posts.sub_lesson_async.all().order_by('pk')
-    data = {
-        'title': posts.lesson,
-        'posts': posts,
-        'lesson': lesson,
-    }
-    return render(request, 'developer/post_async.html', context=data)
+class SubPostViewAsync(ListView):
+    model = SubCategoryAsync
+    template_name = 'developer/post_async.html'
 
+    def get_queryset(self):
+        sub_async = self.kwargs['sub_async']
+        return [get_object_or_404(SubCategoryAsync, slug=sub_async)]
 
-def show_sub_lesson_async(request, sub_async):
-    lesson = get_object_or_404(SubCategoryAsync, slug=sub_async)
-    data = {
-        'title': lesson.lesson,
-        'posts': lesson,
-    }
-    return render(request, 'developer/post_async.html', context=data)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object_list[0]
+        context['title'] = post.lesson
+        context['posts'] = post
+        return context
+
